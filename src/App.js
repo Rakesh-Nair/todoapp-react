645b7b0ba7202729b93652af614218bbdfd5cf87
Rakesh Nair <crucifyrakesh@gmail.com>
2024-06-22 19:11:09 +0530

    Add few function and class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent/>
+      <FourthComponent/>
     </div>
   );
+}
+
+function FirstComponent(){
+  return (
+    <div className='firstComponent'>My First Component</div>
+  );
+}
+
+function SecondComponent(){
+  return (
+    <div className='secondComponent'>My Second Component</div>
+  );
+}
+
+class ThirdComponent extends Component{
+  render(){
+    return <div className='thirdComponent'>My Third Component</div>
+  }
+}
+
+class FourthComponent extends Component{
+  render(){
+    return <div className='fourthComponent'>My Fourth Component</div>
+  }
 }
 
 export default App;
