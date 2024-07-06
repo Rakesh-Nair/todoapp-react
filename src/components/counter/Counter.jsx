1ed8947669b3f89e68951908191681282b63f49a
Rakesh Nair <crucifyrakesh@gmail.com>
2024-07-06 20:06:52 +0530

    Add counter.jsx

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -34,37 +34,3 @@ export default function Counter() {
     </div>
   );
 }
-
-function CounterButton({ by, incrementCount, decrementCount }) {
-  function incrementCounterFunction() {
-    setCount(count + by);
-    incrementCount(by);
-  }
-
-  function decrementCounterFunction() {
-    setCount(count - by);
-    decrementCount(by);
-  }
-
-  const [count, setCount] = useState(0);
-  return (
-    <div className="Counter">
-      <div>
-        <button className="counterButton" onClick={incrementCounterFunction}>
-          +{by}
-        </button>
-        <button className="counterButton" onClick={decrementCounterFunction}>
-          -{by}
-        </button>
-      </div>
-    </div>
-  );
-}
-
-CounterButton.propTypes = {
-  by: PropTypes.number,
-};
-
-CounterButton.defaultProps = {
-  by: 1,
-};
