d0ef9dd44187b45807c1a9feea37c85b6ac0b766
Rakesh Nair <crucifyrakesh@gmail.com>
2024-07-20 15:42:23 +0530

    make login component controlled

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
+import "./TodoApp.css";
+
 export default function TodoApp() {
   return (
     <div className="TodoApp">
-      Todo Management Application
+      {/* Todo Management Application */}
       <LoginComponent />
       {/* <WelcomeComponent /> */}
     </div>
@@ -9,19 +12,43 @@ export default function TodoApp() {
 }
 
 function LoginComponent() {
+  const [username, setUsername] = useState("admin");
+
+  const [password, setPassword] = useState("");
+
+  function changeUserName(event) {
+    setUsername(event.target.value);
+  }
+
+  function changePassword(event) {
+    setPassword(event.target.value);
+  }
+
   return (
     <div className="Login">
       <div className="LoginForm">
         <div>
           <label>User Name</label>
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={changeUserName}
+          />
         </div>
         <div>
           <label>Password</label>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={changePassword}
+          />
         </div>
         <div>
-          <button type="button" name="login">Login</button>
+          <button type="button" name="login">
+            Login
+          </button>
         </div>
       </div>
     </div>
